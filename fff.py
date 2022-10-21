acdcf594ad804d087b5e3e542711383228650c33
kvaditmetal <kvaditmetal@gmail.com>
2022-10-21 23:14:40 +0300

    n

--- fff.py
+++ fff.py
@@ -1,9 +1,7 @@
-# i=0
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# for a in a :
-#     if a <= 5:
-#         i+=1
-#         print(i)
-for elem in a:
-    if elem <= 5:
-        print(elem)
+a = int(input())
+b = int(input())
+counter = 0
+for i in range(a, b):
+    if i**3 == 4 or i**3 == 9 or i**3%10 == 4 or i**3%10==9:
+        counter +=1
+print(counter)
